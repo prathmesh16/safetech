519d7badc44562b79583212393a81c6a8669e00c
Prathmesh Kashinath Jadhav <40669323+prathmesh16@users.noreply.github.com>
2020-12-10 21:54:08 +0530

    Update EmailServer.js

--- EmailServer.js
+++ EmailServer.js
@@ -62,7 +62,7 @@ app.post('/SendEmail', function (req, res) {
 
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
    var host = server.address().address
    var port = server.address().port
    
