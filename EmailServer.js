00afa10787d4a6d51a3847c03b9cd883846c95ae
Prathmesh Kashinath Jadhav <40669323+prathmesh16@users.noreply.github.com>
2020-12-12 16:22:36 +0530

    Add files via upload

--- EmailServer.js
+++ EmailServer.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var cors = require('cors')
+var PORT = process.env.PORT || 8081;
 
 var nodemailer = require('nodemailer');
 
@@ -9,6 +10,16 @@ app.use(cors())
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('Chat server running');
+});
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -60,14 +71,6 @@ app.post('/SendEmail', function (req, res) {
  
 })
 
-
-
-var server = app.listen(process.env.PORT || 8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
 
 
 
